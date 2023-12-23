940d7e531ab95834ef339961e8996af79e1be52c
afuenzalida <a.fuenzalida1494@gmail.com>
2023-12-23 13:07:36 -0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='firedom',
-    version='1.2.0',
+    version='1.3.0',
     description='Simple Firestore ORM',
     url='https://github.com/afuenzalida/firedom',
     author='Andrés Fuenzalida',
